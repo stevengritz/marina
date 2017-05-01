18058687b5118eaa2c7560f92d97313afde549c9
StevenRitz <sgr0003@gmail.com>
2017-04-30 19:24:24 -0700

    update for tests

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 
 # [START main_page]
 class Boat(ndb.Model) :
-	id = ndb.KeyProperty()
+	id = ndb.StringProperty()
 	boatname = ndb.StringProperty()
 	type = ndb.StringProperty()
 	length = ndb.FloatProperty()
@@ -23,11 +23,37 @@ class BoatHandler(webapp2.RequestHandler):
 		new_boat.boatname = boat_data['boatname']
 		new_boat.type = boat_data['type']
 		new_boat.length = boat_data['length']
-		new_boat.at_sea = boat_data['at_sea']
+		new_boat.at_sea = True
 		boat_key = new_boat.put()
-		self.response.write(json.dumps(new_boat.to_dict()))
-		self.response.write('\n')
-		self.response.write(boat_key.string_id())
+		boat_dict = new_boat.to_dict()
+		latest_key = dict()
+		latest_key['boat_id'] = new_boat.key.urlsafe()
+		self.response.write(json.dumps(latest_key))
+		
+	def get(self, id=None):
+		if id:
+			b = ndb.Key(urlsafe=id).get()
+			b_d = b.to_dict()
+			b_d['self'] = "/boat/" + id
+			self.response.write(json.dumps(b_d))
+		#else:
+			
+			
+
+		
+		
+class SlipHandler(webapp2.RequestHandler):
+	def post(self):
+		boat_data = json.loads(self.request.body)
+		new_boat = Boat()
+		new_boat.boatname = boat_data['boatname']
+		new_boat.type = boat_data['type']
+		new_boat.length = boat_data['length']
+		new_boat.at_sea = boat_data['at_sea']
+		new_boat.put()
+		boat_dict = new_boat.to_dict()
+		boat_dict['self'] = '/boat/' + new_boat.key.urlsafe()
+		self.response.write(json.dumps(boat_dict))
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
@@ -40,5 +66,7 @@ webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/boat', BoatHandler),
+	('/boat/(.*)', BoatHandler),
+	('/slip', SlipHandler),
 ], debug=True)
 # [END app]
